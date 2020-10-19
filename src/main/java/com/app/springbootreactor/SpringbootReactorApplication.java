024d222a0e051e847a2900d789011c3fa4644f2d
jsovalles <jsovalles95@gmail.com>
2020-10-19 11:55:00 -0500

    agregando dependencias de reactive stream y colocando un ejemplo de un Observable (Flux)

--- src/main/java/com/app/springbootreactor/SpringbootReactorApplication.java
+++ src/main/java/com/app/springbootreactor/SpringbootReactorApplication.java
@@ -1,13 +1,21 @@
 package com.app.springbootreactor;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringbootReactorApplication {
+public class SpringbootReactorApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootReactorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootReactorApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Flux<String> nombres = Flux.just("Pedro", "Juan", "Jose")
+                .doOnNext(System.out::println);
+        nombres.subscribe();
+    }
 }
